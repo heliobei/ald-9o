56fadba7c0507c67248a380d85a9ede520ed2379
heliobei <suporte@beieducacaopedagogico.com>
2024-10-29 15:12:20 -0300

    Update AccordionContent.jsx

--- app/components/AccordionContent.jsx
+++ app/components/AccordionContent.jsx
@@ -21,7 +21,7 @@ export default function Acc() {
                 <iframe 
                     width="560" 
                     height="315" 
-                    src="https://www.youtube.com/embed/Hw0lZjIxELY" 
+                    src="https://youtu.be/K0rGpw0L-d0" 
                     title="YouTube video player" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
@@ -33,7 +33,7 @@ export default function Acc() {
             </AccordionItem>
             <AccordionItem key="2" aria-label="Caderno do Estudante" subtitle="Clique para abrir" title="Manual de Acesso à Plataforma" className="hover:bg-sky-100 transition-colors duration-200 rounded-lg px-4 font-bold dark:hover:bg-sky-700 py-4 my-4 bg-slate-200">
              <div className='flex justify-center items-center'>
-                <iframe src="https://beieducacao.com.br/conteudo-aprendendo-a-lidar-com-dinheiro/assets/uploads/roteiro.pdf" width="100%" height="600" seamless="seamless" scrolling="no" frameBorder="0" allowFullScreen allow="autoplay; clipboard-read; clipboard-write"></iframe>
+                <iframe src="https://drive.google.com/file/d/1W2qDAF5omndVyFoawPBIX35Y3Es3vDEu/view?usp=sharing" width="100%" height="600" seamless="seamless" scrolling="no" frameBorder="0" allowFullScreen allow="autoplay; clipboard-read; clipboard-write"></iframe>
                 </div>
             </AccordionItem>
             
